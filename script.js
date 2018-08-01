010ab38ece0f51518452d5ea59a2063602d1071d
Merlin Nimier-David <merlin.nimier@gmail.com>
2018-08-01 18:50:04 +0200

    MathJax import and configuration working

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
  * Script to be injected into the electron app.
  */
 
+function triggerMath() {
+    if (MathJax !== undefined) {
+        MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
+    }
+}
+
 document.addEventListener("keydown", function (e) {
     if (e.which === 123) {
         // F12
@@ -11,8 +17,56 @@ document.addEventListener("keydown", function (e) {
         location.reload();
     } else if (e.which === 79) {
         // O
-        console.log("It works!");
+        triggerMath();
     }
 });
 
-console.log("We're in.");
+// Adapted from:
+// https://github.com/fsavje/math-with-slack
+document.addEventListener('DOMContentLoaded', function() {
+    var mathjax_config = document.createElement('script');
+    mathjax_config.type = 'text/x-mathjax-config';
+    mathjax_config.text = "                                                           \
+    MathJax.Hub.Config({                                                              \
+      messageStyle: 'none',                                                           \
+      extensions: ['tex2jax.js'],                                                     \
+      jax: ['input/TeX', 'output/HTML-CSS'],                                          \
+      tex2jax: {                                                                      \
+        displayMath: [['\$\$', '\$\$']],                                              \
+        element: 'msgs_div',                                                          \
+        ignoreClass: 'ql-editor',                                                     \
+        inlineMath: [['\$', '\$']],                                                   \
+        processEscapes: true,                                                         \
+        skipTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']          \
+      },                                                                              \
+      TeX: {                                                                          \
+        extensions: ['AMSmath.js', 'AMSsymbols.js', 'noErrors.js', 'noUndefined.js']  \
+      }                                                                               \
+    });                                                                               \
+    ";
+
+    // TODO: MathJax observer for Trello. Requires findings events for:
+    // - Card opened / closed
+    // - Board opened / closed
+    // - Card description done editing
+
+    // var mathjax_observer = document.createElement('script');
+    // mathjax_observer.type = 'text/x-mathjax-config';
+    // mathjax_observer.text = `
+    //     var target = document.querySelector('#messages_container');
+    //     var options = { attributes: false, childList: true, characterData: true, subtree: true };
+    //     var observer = new MutationObserver(function (r, o) { MathJax.Hub.Queue(['Typeset', MathJax.Hub]); });
+    //     observer.observe(target, options);
+    // `;
+
+    var mathjax_script = document.createElement('script');
+    mathjax_script.type = 'text/javascript';
+    mathjax_script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-MML-AM_CHTML';
+
+    document.head.appendChild(mathjax_config);
+    // document.head.appendChild(mathjax_observer);
+    document.head.appendChild(mathjax_script);
+
+    console.log("MathJax scripts added.");
+    triggerMath();
+});
